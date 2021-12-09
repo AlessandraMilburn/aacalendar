f6f0fa142106fd0e2235c1aabe54eb4e8d32eaf0
Alessandra Milburn <alessandra.milburn@yahoo.com>
2021-12-09 09:15:34 +0400

    imported styles, months array and added a line for current date to render

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,15 @@
+import styles from '../styles/Calendar.module.css';
+import { months } from "../utils/calendar.utils"
+
+const today = new Date();
+const firstDayOfYear = new Date(today.getFullYear(), 0, 1);
+const pastDaysOfYear = (today - firstDayOfYear) / 86400000;
+
 export default function Header() {
-    return (
-        <h1>Calendar</h1>
-    )
+  return (
+    <div className={styles.header}>
+      <h1>Calendar</h1>
+      <h4>{today.getDate()} {months[today.getMonth()]} {today.getFullYear()}</h4>
+    </div>
+  )
 }
